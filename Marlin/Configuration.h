eca4dc63b7ac74781a0e723f9f1aeb5c38c09d40
thiemok <krausethiemo@googlemail.com>
2019-12-14 13:19:04 +0100

    feat(z Probe): set correct servo angles

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -722,7 +722,7 @@
  * Z Servo Probe, such as an endstop switch on a rotating arm.
  */
 #define Z_PROBE_SERVO_NR 0   // Defaults to SERVO 0 connector.
-#define Z_SERVO_ANGLES {70,0}  // Z Servo Deploy and Stow angles
+#define Z_SERVO_ANGLES {85,0}  // Z Servo Deploy and Stow angles
 
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
